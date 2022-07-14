36d36ab9645a39af25a13d562be22e33dc70db4d
oshino <B1747322151@outlook.com>
2022-07-14 16:58:33 +0800

    feat: leetcoding

---
+++ src/15.三数之和.ts
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=typescript
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+function threeSum(nums: number[]): number[][] {
+  const length = nums.length;
+  if (length < 3) return [];
+  const result: number[][] = [];
+  // 升序排序
+  nums.sort((a, b) => a - b);
+
+  // 遍历每一个值，为 nums[i] 找到另外两个数使得三数之和为 0
+  for (let i = 0; i < length; i++) {
+    // nums[i] 已经大于 0 了，说明右边的数肯定大于 0 了，不可能找到更多的三元组，提前返回
+    if (nums[i] > 0) return result;
+    // 跳过重复的 nums[i]
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    // 双指针，对 num[i] 右边的区域进行两边向中间的遍历
+    let [left, right] = [i + 1, length - 1];
+    while (left < right) {
+      if (nums[i] + nums[left] + nums[right] === 0) {
+        // 满足要求的三元组
+        result.push([nums[i], nums[left], nums[right]]);
+        // 跳过 left、right 指针重复值
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+
+        left++;
+        right--;
+      } else if (nums[i] + nums[left] + nums[right] > 0) {
+        // left->right是递增的，这里说明 nums[right] 太大了，需要左移
+        right--;
+      } else {
+        // left->right是递增的，这里说明 nums[left] 太小了，需要右移
+        left++;
+      }
+    }
+  }
+
+  return result;
+}
+// @lc code=end
---
+++ src/16.最接近的三数之和.ts
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=typescript
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+/** 和【15.三数之和】类似，使用一个变量记录距离 target 最近的和，并且在遍历过程中更新 */
+function threeSumClosest(nums: number[], target: number): number {
+  const length = nums.length;
+  nums.sort((a, b) => a - b);
+
+  let result = +Infinity;
+
+  for (let i = 0; i < length; i++) {
+    // 跳过重复的 nums[i]
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let [left, right] = [i + 1, length - 1];
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === target) return target;
+      // 更新最近和
+      if (Math.abs(sum - target) < Math.abs(result - target)) {
+        result = sum;
+      }
+
+      if (sum > target) {
+        // left->right是递增的，这里说明 nums[right] 太大了，需要左移，顺便跳过重复值
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        right--;
+      } else {
+        // left->right是递增的，这里说明 nums[left] 太小了，需要右移，顺便跳过重复值
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        left++;
+      }
+    }
+  }
+
+  return result;
+}
+// @lc code=end
